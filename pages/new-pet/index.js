9275cbc0495796022a337e410b50630f919923f6
Martin Soler <martinsoler_91@outlook.com>
2023-06-27 13:49:20 -0300

    add useSession in protected page to check for auth users

--- pages/new-pet/index.js
+++ pages/new-pet/index.js
@@ -1,7 +1,10 @@
 import { useRouter } from "next/router";
 import NewLostPetForm from "../../components/petsComponents/AddLostPetForm/NewLostPetForm";
+import { useSession } from "next-auth/react";
 
 const NewLostPetPage = () => {
+  const { data: session, status } = useSession();
+  console.log(session, status);
   const router = useRouter();
   const addPetHandler = async (enteredpetData) => {
     const response = await fetch("/api/new-pet", {
